3de9f1b1d10b7f90bbccaf27180e1d148ba9ee3a
KINGBOTs1 <ds767365@gmail.com>
2023-06-16 03:58:48 +0800

    ss

--- routes/SearchRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { search } from '../controllers/SearchController.js';
-
-const router = express.Router();
-
-router.get('/search', search);
-
-export default router;
