de3ac461a41a17cb7bce00b0dbf96afafee75f21
KINGBOTs1 <ds767365@gmail.com>
2023-06-17 02:49:57 +0800

    dd

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,42 +4,28 @@ import User from '../models/UserModel.js';
 const UserController = {
   login: async (req, res) => {
     try {
-      const {
-        email,
-        nim,
-        password
-      } = req.body;
+      const { email, nim, password } = req.body;
       let user;
 
       if (email) {
         // Jika email tersedia, cari pengguna berdasarkan email
-        user = await User.findOne({
-          email
-        });
+        user = await User.findOne({ email });
       } else if (nim) {
         // Jika nim tersedia, cari pengguna berdasarkan nim
-        user = await User.findOne({
-          nim
-        });
+        user = await User.findOne({ nim });
       } else {
         // Jika email dan nim tidak tersedia, kembalikan respons error
-        return res.status(400).json({
-          message: 'Please provide email or NIM'
-        });
+        return res.status(400).json({ message: 'Please provide email or NIM' });
       }
 
       if (!user) {
-        return res.status(401).json({
-          message: 'Invalid credentials'
-        });
+        return res.status(401).json({ message: 'Invalid credentials' });
       }
 
       const passwordMatch = await bcrypt.compare(password, user.password);
 
       if (!passwordMatch) {
-        return res.status(401).json({
-          message: 'Invalid credentials'
-        });
+        return res.status(401).json({ message: 'Invalid credentials' });
       }
 
       if (user.role === 'admin') {
@@ -57,32 +43,18 @@ const UserController = {
       }
     } catch (error) {
       console.error('Error during login:', error);
-      return res.status(500).json({
-        message: 'Internal server error'
-      });
+      return res.status(500).json({ message: 'Internal server error' });
     }
   },
 
   register: async (req, res) => {
     try {
-      const {
-        email,
-        password,
-        role,
-        nama,
-        nim,
-        jurusan,
-        fakultas
-      } = req.body;
+      const { email, password, role, nama, nim, jurusan, fakultas } = req.body;
 
       // Cek apakah pengguna dengan email tersebut sudah ada di database
-      const existingUser = await User.findOne({
-        email
-      });
+      const existingUser = await User.findOne({ email });
       if (existingUser) {
-        return res.status(400).json({
-          message: 'Email is already registered'
-        });
+        return res.status(400).json({ message: 'Email is already registered' });
       }
 
       // Hash password sebelum disimpan ke database
@@ -100,43 +72,24 @@ const UserController = {
       });
 
       if (newUser) {
-        return res.status(201).json({
-          message: 'User successfully registered'
-        });
+        return res.status(201).json({ message: 'User successfully registered' });
       } else {
-        return res.status(500).json({
-          message: 'Failed to register user'
-        });
+        return res.status(500).json({ message: 'Failed to register user' });
       }
     } catch (error) {
       console.error('Error during registration:', error);
-      return res.status(500).json({
-        message: 'Internal server error'
-      });
+      return res.status(500).json({ message: 'Internal server error' });
     }
   },
-
+  
   getUser: async (req, res) => {
     try {
-      const {
-        userId
-      } = req.params;
-
-      // Cari pengguna berdasarkan ID
+      const userId = mongoose.Types.ObjectId(req.params.id); // Mengonversi string menjadi Object ID
       const user = await User.findById(userId);
-
-      if (!user) {
-        return res.status(404).json({
-          message: 'User not found'
-        });
-      }
-
-      // Mengembalikan data pengguna
-      return res.status(200).json(user);
+      res.json(user);
     } catch (error) {
-      console.error('Error while getting user:', error);
-      return res.status(500).json({
-        message: 'Internal server error'
+      res.status(500).json({
+        error: error.message
       });
     }
   }
