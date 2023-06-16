4547c339b9eb8c1f86d965f9eb56b2a46d72236a
KINGBOTs1 <ds767365@gmail.com>
2023-06-16 15:05:21 +0800

    ddd

--- index.js
+++ index.js
@@ -1,32 +1,60 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-import db from './config/Database.js';
-import userRoutes from './routes/userRoutes.js';
+import FileUpload from 'express-fileupload';
+import cors from 'cors';
+import sequelize from './config/Database.js';
+//ROUTES
+import ReportRoute from './routes/ReportRoute.js';
+import LikeRoute from './routes/LikeRoute.js';
+import SearchRoute from './routes/SearchRoute.js';
+import UserRoutes from './routes/UserRoute.js';
+//MODELS
+import Report from './models/ReportModel.js';
+import Like from './models/LikeModel.js';
+import User from './models/UserModel.js';
+import dotenv from "dotenv";
 
-const app = express();
-const port = process.env.PORT || 3000;
-
-// Load environment variables
 dotenv.config();
 
-// Test database connection
-db.authenticate()
-  .then(() => {
-    console.log('Database connection has been established successfully.');
-  })
-  .catch((error) => {
-    console.error('Unable to connect to the database:', error);
-  });
+const app = express();
 
-app.use(bodyParser.urlencoded({
-  extended: true
+app.use(cors());
+app.use(express.json());
+app.use(FileUpload());
+app.use(express.static('public'));
+app.use(ReportRoute);
+app.use(LikeRoute);
+app.use(SearchRoute);
+app.use(UserRoutes);
+
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Koneksi ke database berhasil terhubung');
+
+    // Sinkronisasi struktur tabel
+    await sequelize.sync({ alter: true });
+    console.log('Struktur tabel berhasil disinkronisasi');
+
+    // Lakukan hubungan antara model Report dan Like
+    Report.hasMany(Like, { foreignKey: 'reportId' });
+    Like.belongsTo(Report, { foreignKey: 'reportId' });
+  } catch (error) {
+    console.error('Gagal terhubung ke database:', error);
+  }
+})();
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Koneksi ke database berhasil terhubung');
+  } catch (error) {
+    console.error('Gagal terhubung ke database:', error);
+  }
+})();
+app.use(cors({
+  origin: process.env.ORIGIN, // Atur origin sesuai dengan URL frontend React
+  credentials: true, // Jika Anda mengizinkan pengiriman cookie atau header lain dalam permintaan
 }));
-app.use(bodyParser.json());
 
-// Register routes
-app.use('/api/users', userRoutes);
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.listen(process.env.PORT, () => console.log('Server Sedang Berjalan...'));
